3ee1303b008cf08f1a816e8e2afef0153cedfa9a
moshe084 <moshe084@gmail.com>
2024-12-01 14:19:04 +0200

    Initialize React project with Bitcoin Dashboard

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BitcoinDashboard from './components/BitcoinDashboard';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-8">
+      <BitcoinDashboard />
+    </div>
+  );
+}
+
+export default App;
